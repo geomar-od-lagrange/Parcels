584a20b2cba031a52a8e4b6a4d9f77fe0f5a1519
Joe Schoonover <joe@fluidnumerics.com>
2025-11-11 20:51:58 -0500

    Add stommel dataset consistent with icon c-grid layout

--- src/parcels/_datasets/unstructured/generic.py
+++ src/parcels/_datasets/unstructured/generic.py
@@ -111,6 +111,76 @@ def _stommel_gyre_delaunay():
     return ux.UxDataset({"U": u, "V": v, "W": w, "p": p}, uxgrid=uxgrid)
 
 
+def _stommel_gyre_delaunay_icon():
+    """
+    Stommel gyre on a Delaunay grid. the naming convention of the dataset and grid is consistent with what is
+    provided by UXArray when reading in ICON datasets.
+    This dataset is a single vertical layer of a barotropic ocean gyre on a square domain with closed boundaries.
+    The velocity field provides a slow moving interior circulation and a western boundary current. All fields are placed
+    on the vertices of the grid and at the element vertical faces.
+    """
+    lon, lat = np.meshgrid(np.linspace(0, 60.0, Nx, dtype=np.float32), np.linspace(0, 60.0, Nx, dtype=np.float32))
+    lon_flat = lon.ravel()
+    lat_flat = lat.ravel()
+    zf = np.linspace(0.0, 1000.0, 2, endpoint=True, dtype=np.float32)  # Vertical element faces
+    zc = 0.5 * (zf[:-1] + zf[1:])  # Vertical element centers
+    nz1 = zc.size
+
+    # mask any point on one of the boundaries
+    mask = (
+        np.isclose(lon_flat, 0.0) | np.isclose(lon_flat, 60.0) | np.isclose(lat_flat, 0.0) | np.isclose(lat_flat, 60.0)
+    )
+
+    boundary_points = np.flatnonzero(mask)
+
+    uxgrid = ux.Grid.from_points(
+        (lon_flat, lat_flat),
+        method="regional_delaunay",
+        boundary_points=boundary_points,
+    )
+    uxgrid.attrs["Conventions"] = "UGRID-1.0"
+
+    # Define arrays U (zonal), V (meridional) and P (sea surface height)
+    U = np.zeros((1, nz1, uxgrid.n_edge), dtype=np.float64)  # edge center, vertically centered
+    V = np.zeros((1, nz1, uxgrid.n_edge), dtype=np.float64)  # edge center, vertically centered
+
+    for i, (x, y) in enumerate(zip(uxgrid.edge_lon, uxgrid.edge_lat, strict=False)):
+        xi = x / 60.0
+        yi = y / 60.0
+
+        U[0, 0, i] = -vmax * (1 - math.exp(-xi / delta) - xi) * np.cos(math.pi * yi)
+        V[0, 0, i] = vmax * ((2.0 - xi) * math.exp(-xi / delta) - 1) * np.sin(math.pi * yi)
+
+    u = ux.UxDataArray(
+        data=U,
+        name="U",
+        uxgrid=uxgrid,
+        dims=["time", "nz1", "n_edge"],
+        coords=dict(
+            time=(["time"], [TIME[0]]),
+            nz1=(["nz1"], zc),
+        ),
+        attrs=dict(
+            description="zonal velocity", units="m/s", location="node", mesh="delaunay", Conventions="UGRID-1.0"
+        ),
+    )
+    v = ux.UxDataArray(
+        data=V,
+        name="V",
+        uxgrid=uxgrid,
+        dims=["time", "nz1", "n_edge"],
+        coords=dict(
+            time=(["time"], [TIME[0]]),
+            nz1=(["nz1"], zc),
+        ),
+        attrs=dict(
+            description="meridional velocity", units="m/s", location="node", mesh="delaunay", Conventions="UGRID-1.0"
+        ),
+    )
+
+    return ux.UxDataset({"U": u, "V": v}, uxgrid=uxgrid)
+
+
 def _fesom2_square_delaunay_uniform_z_coordinate():
     """
     Delaunay grid with uniform z-coordinate, mimicking a FESOM2 dataset.
@@ -315,4 +385,5 @@ datasets = {
     "stommel_gyre_delaunay": _stommel_gyre_delaunay(),
     "fesom2_square_delaunay_uniform_z_coordinate": _fesom2_square_delaunay_uniform_z_coordinate(),
     "fesom2_square_delaunay_antimeridian": _fesom2_square_delaunay_antimeridian(),
+    "stommel_gyre_delaunay_icon": _stommel_gyre_delaunay_icon(),
 }
